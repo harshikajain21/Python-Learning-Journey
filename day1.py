ce86fa48df1c24caad48de67093ddc5029d28d43
Harshika Jain <harshi21jain1105@gmail.com>
2026-05-15 15:03:50 +0530

    Learning print functions and patterns

---
+++ day1.py
@@ -0,0 +1,69 @@
+#print command
+
+print("Hello world")
+
+
+#pratice q1 #Square from print
+print ("+------------+")
+print ("|            |")
+print ("|            |")
+print ("|            |")
+print ("|            |")
+print ("|            |")
+print ("+------------+")
+or 
+print ("+"+"-"*10+"+")
+print(("|"+" "*10+"|")*5, end="")
+print ("+"+"-"*10+"+")
+
+/n
+
+# 1. Square Hollow Pattern
+
+print("*"*6)
+print(("*" + " "*4 + "*\n") * 4, end="")
+print("*"*6)
+
+/n
+
+# 2. Square Fill Pattern
+
+print(("*"*7 + "\n") * 6)
+
+/n
+
+# 3. Hollow Rectangle
+
+print("#"*6)
+print(("#" + " "*4 + "#\n") * 3, end="")
+print("#"*6)
+
+/n
+
+# 4. Filled Square using #
+
+print(("#"*8 + "\n") * 6)
+
+/n
+
+# 5. Number Triangle Pattern
+
+print("1")
+print("2 "*2)
+print("3 "*3)
+print("4 "*4)
+print("5 "*5)
+print("6 "*6)
+
+/n
+
+# 6. Up Pointing Arrow
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print(" *****")
