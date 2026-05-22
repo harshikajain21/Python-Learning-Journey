77e4419fa7f73c95aaa4fb4f497f46dab591b25f
Harshika Jain <harshi21jain1105@gmail.com>
2026-05-22 15:06:26 +0530

    Add initial implementation of list operations in day6.py

---
+++ day6.py
@@ -0,0 +1,51 @@
+'''treversing list '''
+
+# list = [1,2,3,4,5,6,7,8,9,10]
+# for itration in range(len(list)):
+#      print(list[itration])
+
+'''write a program to insert 10 element from 1 to 20 to a list '''
+
+# list_number =[]
+# for iterator in range(10):
+#     # list_number.append(i)
+#     # list_number.insert(iterator,iterator)
+#     list_number.append(iterator+1)
+# print(list_number)
+
+'''write program to update existing list'''
+
+# my_list= [10,20,30,40,50,60,70,80,90,100]
+
+# for index in range(len(my_list)):
+#   my_list[index] +=1
+# print(my_list)    
+
+'''write program to print sum of all element in the list'''
+# my_list= [10,20,30,40,50,60,70,80,90,100]
+# sum =0
+# for element in range(len(my_list)):
+#    sum += my_list[element]
+# print(sum)                   
+
+# my_list=[1,2,3,4,5]
+# total = 0
+# for element in my_list:
+#  total += element                         
+# print(my_list)
+# print(total)
+
+#making two variable equal
+# var1 = 1
+# var2 = 2
+
+# print("var1:",var1)
+# print("var2:",var2)
+
+# var1 ,var2 = var2 ,var1
+# print("var1",var1)
+# print("var2",var2)
+
+
+# list = [1,2,3,4,5,6,7]
+# list[1],list[3] = list[3] ,list[1]   #use to swap the values without loss
